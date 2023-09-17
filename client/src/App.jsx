ac2e3e969a8ccccebb279027727c8a62a75fce2c
sujay000 <sujaysapkal22@gmail.com>
2023-09-17 17:21:06 +0530

    add create blog functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from '../components/Login'
 import SignUp from '../components/SignUp'
 import Dashboard from '../components/Dashboard'
+import CreateBlog from '../components/CreateBlog'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/dashboard/*" element={<Dashboard />} />
+                <Route path="/createblog" element={<CreateBlog />} />
             </Routes>
         </div>
     )
