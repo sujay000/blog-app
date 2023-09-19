3fff933f51d14a35dcf1a4c0aa0a2300d67307cd
sujay000 <sujaysapkal22@gmail.com>
2023-09-19 12:04:59 +0530

    frontend prettify
    use mui to prettify FE

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Link from '@mui/material/Link'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Stack from '@mui/material/Stack'
 import { useNavigate } from 'react-router-dom'
-const baseurl = `https://blog-app-27r2.onrender.com`
 
-function Login() {
+// const baseurl = `https://blog-app-27r2.onrender.com`
+const baseurl = `http://localhost:3000`
+
+const defaultTheme = createTheme()
+
+export default function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
+
     async function handleLogin() {
         const url = baseurl + `/login`
         console.log(username, 'here')
@@ -33,6 +49,7 @@ function Login() {
             alert(`login invalid : ${res.message}`)
         }
     }
+
     async function handleDummy() {
         setUsername('hello')
         setPassword('123')
@@ -57,20 +74,89 @@ function Login() {
             alert(`login invalid : ${res.message}`)
         }
     }
+
     return (
-        <div>
-            <h2>Login</h2>
-            Username - <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} /> <br />
-            Password - <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
-            <br />
-            <button onClick={handleLogin} id="loginButton">
-                Login
-            </button>{' '}
-            <br />
-            {/* <button onClick={handleDummy}>Use dummy account</button> <br /> */}
-            <Link to={'/signup'}>Dont have an acc? Signup</Link>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Login
+                        </Typography>
+                        <Box sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Stack
+                                direction="row"
+                                spacing={2}
+                                alignItems="center"
+                                justifyContent="center"
+                                sx={{ mt: 3, mb: 3 }}
+                            >
+                                <Button variant="contained" fullWidth onClick={handleLogin}>
+                                    Login
+                                </Button>
+                                <Button variant="contained" fullWidth onClick={handleDummy}>
+                                    Dummy Account
+                                </Button>
+                            </Stack>
+
+                            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <Link href="/signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </div>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
     )
 }
-
-export default Login
--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Link from '@mui/material/Link'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Stack from '@mui/material/Stack'
 import { useNavigate } from 'react-router-dom'
-const baseurl = `https://blog-app-27r2.onrender.com`
 
-function SignUp() {
+// const baseurl = `https://blog-app-27r2.onrender.com`
+const baseurl = `http://localhost:3000`
+
+const defaultTheme = createTheme()
+
+export default function SignUp() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
+
     async function handleSignUp() {
         const url = baseurl + `/signup`
         let data = {
@@ -31,15 +47,85 @@ function SignUp() {
     }
 
     return (
-        <div>
-            <h2>Sign Up</h2>
-            Username - <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} /> <br />
-            Password - <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />{' '}
-            <br />
-            <button onClick={handleSignUp}>Sign Up</button> <br />
-            <Link to="/">Already have an acc? Login</Link>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign Up
+                        </Typography>
+                        <Box sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Stack
+                                direction="row"
+                                spacing={2}
+                                alignItems="center"
+                                justifyContent="center"
+                                sx={{ mt: 3, mb: 3 }}
+                            >
+                                <Button variant="contained" fullWidth onClick={handleSignUp}>
+                                    Sign Up
+                                </Button>
+                            </Stack>
+
+                            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <Link href="/" variant="body2">
+                                    {'Already have an account? Login'}
+                                </Link>
+                            </div>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+            </Grid>
+        </ThemeProvider>
     )
 }
-
-export default SignUp
